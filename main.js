8691a2e365a72d0fa9c1e4eac31b3ddcc29444fd
Daboosy <72225518+daboosy@users.noreply.github.com>
2020-10-02 17:20:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const clinet = new Discord.client();
+
+
+
+client.once('ready', () => {
+    console.log('Aiko is online!');
+});
+
+
+
+clinet.login('NzYxNTU3NDA2NDgzNjExNjQ4.X3cVmg.XGTJ2-SgKNQ_smHG2Ln3ES3Krio');

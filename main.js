43e21fe48d9a404e98dff41e94ef2d5f1d6aea31
Daboosy <72225518+daboosy@users.noreply.github.com>
2020-10-02 17:29:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,38 @@ const Discord = require('discord.js');
 
 const clinet = new Discord.client();
 
+const prefix = '-';
 
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFilles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+ for(const file of commandFilles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('Aiko is online!');
 });
+
+client.on('message', message =>{
+    if(!message.content.startWith(prefix) || message.auther.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } else if (command == 'youtube'){
+        client.commands.get('youtube').execute(message, args);
+    }
+})
+
+
+
 
 
 
